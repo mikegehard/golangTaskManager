3ae78db05b67825c32539d8954dc6dbae7ad31f3
Mike Gehard <mike.gehard@gmail.com>
2014-09-28 09:49:57 -0600

    Get a basic ginkgo test working

---
+++ main/main_test.go
@@ -0,0 +1,28 @@
+package main_test
+
+import (
+	"github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/gexec"
+	"os/exec"
+)
+
+var Start = gexec.Start
+var Say = gbytes.Say
+var Build = gexec.Build
+var Exit = gexec.Exit
+
+var _ = Describe("The greeting", func() {
+	AfterSuite(func() {
+		gexec.CleanupBuildArtifacts()
+	})
+
+	It("should be show when cli is run", func() {
+		path, err := Build("github.com/msgehard/golangTaskManager/main")
+		Expect(err).NotTo(HaveOccurred())
+
+		cli, err := Start(exec.Command(path), GinkgoWriter, GinkgoWriter)
+		Expect(err).NotTo(HaveOccurred())
+
+		Eventually(cli).Should(Exit(0))
+	})
+})
